e21783346ccea0069587b8571febb7583aa2a60f
Karthik <Shaji>
2021-02-20 23:22:59 -0800

    Added the paths the drones will take

--- Code/drawGraphWithDrones.m
+++ Code/drawGraphWithDrones.m
@@ -22,7 +22,17 @@ function [   ]  = drawGraphWithDrones(graph, droneLocX, droneLocY)
         Y = [graph.node(:).y ];
         plot(X,Y, 'ok', 'MarkerSize', 10, 'MarkerEdgeColor' , 'r' , 'MarkerFaceColor' , [ 1, 0.6 , 0.6]);
     end
-    
+    for i = 1: length(droneLocX)
+        x1 = droneLocX(i);
+        y1 = droneLocY(i);
+        for j = 1: graph.n
+            x2 = graph.node(j).x;
+            y2 = graph.node(j).y;
+            X = [x1, x2];
+            Y = [y1, y2];
+            plot(X, Y, ':k');
+        end
+    end
     for i = 1: length(droneLocX)
         X = [droneLocX(i)];
         Y = [droneLocY(i)];
