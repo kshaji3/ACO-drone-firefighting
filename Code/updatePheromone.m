6a1b47f0bedeecbd72fc0ad5456c5e34088e6543
Karthik <Shaji>
2021-03-01 22:25:56 -0800

    One iteration!

--- Code/updatePheromone.m
+++ Code/updatePheromone.m
@@ -1,12 +1,12 @@
-function [ colony ] = updatePheromone(tau, droneNo, colony)
+function [ tau ] = updatePheromone(tau, droneNo, colony)
 
     nodeNo = length(colony(droneNo).ant(1).tour);
     antNo = length(colony(droneNo).ant(:));
 
     for i = 1 : antNo % for each ant
         for j = 1 : nodeNo-1 % for each node in the tour
-            currentNode = colony.ant(i).tour(j);
-            nextNode = colony.ant(i).tour(j+1);
+            currentNode = colony(droneNo).ant(i).tour(j);
+            nextNode = colony(droneNo).ant(i).tour(j+1);
         
             tau(currentNode, nextNode, droneNo) = tau(currentNode, nextNode, droneNo)  + 1./ colony(droneNo).ant(i).fitness;
             tau(nextNode, currentNode, droneNo) = tau(nextNode, currentNode, droneNo)  + 1./ colony(droneNo).ant(i).fitness;
