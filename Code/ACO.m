c7c3d751ae71e09a4c5f23bef8592b4b41f138b7
Karthik <Shaji>
2021-03-01 22:36:26 -0800

    Commenting segments

--- Code/ACO.m
+++ Code/ACO.m
@@ -5,20 +5,21 @@ clc
 %get the fires and drones
 [fireIntensity, fireLocX, fireLocY] = createFires();
 [droneCapac, droneLocX, droneLocY] = createDrones();
+
 %Create the graph
 [graph] = createGraph(fireLocX, fireLocY);
+
 %Draw the graph
 figure
 subplot(1, 4, 1)
 drawGraph(graph);
 subplot(1, 4, 2);
 drawGraphWithDrones(graph, droneLocX, droneLocY);
-%% ACO Algorithm
 
 %% Initial Parameters
 maxIter = 1;
 antNo = 10;
-droneNo = 5;
+droneNo = 5; %agents in CVRP
 
 tau0 = 10 * 1 / (graph.n * mean(graph.edges(:) ) );
 eta = 1 ./ graph.edges; %edge desirability
@@ -41,11 +42,13 @@ colony = [];
 %colony = zeros(0, droneNo);
 allAntsFitness = zeros(0, droneNo);
 for i = 1: maxIter
+    
     %create ants
     for j = 1: droneNo
         
         colony = createColonies(graph, j, colony, antNo, tau(:,:,j), eta, alpha, beta) %works
-        for k = 1: antNo %calculate fitnesses
+        for k = 1: antNo 
+            %calculate fitnesses of all ants in a specific drone ant colony
             colony(j).ant(k).fitness = fitnessFunction(colony(j).ant(k).tour , graph);
         end
         allAntsFitness = [colony(j).ant(:).fitness];
@@ -54,9 +57,14 @@ for i = 1: maxIter
             bestFitness = colony(j).ant(minIndex).fitness;
             bestTour = colony(j).ant(minIndex).tour;
         end
+        %Find the best ant of this colony
         colony(j).queen.tour = bestTour;
         colony(j).queen.fitness = bestFitness;
+        
+        %Update pheromone matrix
         tau = updatePheromone(tau, j, colony);
+        
+        %Evaporation
         tau(:, :, j) = (1 - rho) .* tau(:, :, 1);
         outmsg = ['Iteration #' , num2str(i), ' Shortest Length = ' , num2str(colony(j).queen.fitness) ];
         disp(outmsg)
@@ -64,6 +72,8 @@ for i = 1: maxIter
         title(['Iteration #' , num2str(i) ])
         subplot(1, 4, 3)
         cla
+        
+        %Visualize best tour and pheromone concentration
         drawBestTour(colony(j), graph);
         subplot(1, 4, 4)
         cla
