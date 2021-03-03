f3b2ddafcb8758564d7cd85284ed2af7cf827df4
Karthik <Shaji>
2021-03-03 14:29:56 -0800

    Graphing Function

--- Code/drawBestTour.m
+++ Code/drawBestTour.m
@@ -1,24 +1,6 @@
 function [ ] = drawBestTour(colony , droneNo, graph)
     queenTour = colony.queen.tour;
     hold on
-    
-    for i = 1 : length(queenTour) - 1
-    
-        currentNode = queenTour(i);
-        nextNode =  queenTour(i+1);
-    
-        x1 = graph.node(currentNode).x;
-        y1 = graph.node(currentNode).y;
-    
-        x2 = graph.node(nextNode).x;
-        y2 = graph.node(nextNode).y;
-    
-        X = [x1 , x2];
-        Y = [y1, y2];
-        plot (X, Y, '-r');
-
-    end
-
 
     for i = 1 : graph.n
     
@@ -27,7 +9,34 @@ function [ ] = drawBestTour(colony , droneNo, graph)
     
         plot(X, Y, 'ok', 'markerSize' , 10 , 'MarkerEdgeColor' , 'r' , 'MarkerFaceColor', [1, 0.6, 0.6]);
     end
+    
+    color = [0.1 * droneNo, 0.5, 0.5];
+    
+    if (length(queenTour) == 1)
+        currentNode = queenTour(1);
+    
+        x1 = graph.node(currentNode).x;
+        y1 = graph.node(currentNode).y;
+        plot (x1, y1, 'color', color);
+        
+    else
+        for i = 1 : length(queenTour) - 1
+    
+            currentNode = queenTour(i);
+            nextNode =  queenTour(i+1);
+    
+            x1 = graph.node(currentNode).x;
+            y1 = graph.node(currentNode).y;
+    
+            x2 = graph.node(nextNode).x;
+            y2 = graph.node(nextNode).y;
+    
+            X = [x1 , x2];
+            Y = [y1, y2];
+            plot (X, Y, 'color', color);
 
+        end
+    end
     title('Best tour (the queen)')
     box('on');
 end
