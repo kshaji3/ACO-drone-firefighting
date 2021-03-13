9c01e9159cbf272d1353a34d518d7cb63b654638
Karthik <Shaji>
2021-03-12 19:17:42 -0800

    Added Variables
    -Significantly increases runtime

--- Code/fireFitnessFunction.m
+++ Code/fireFitnessFunction.m
@@ -1,8 +1,12 @@
-function [ fireFitness ] = fireFitnessFunction (ant, droneCapac, totalDrones, totalFires)
-    fireFitness = 0.01 + fireSumFitness(droneCapac, ant.fireSum);
+function [ fireFitness, fireTotDiffFit, fireEqFit, fireIntFit ] = fireFitnessFunction (ant, droneCapac, totalDrones, totalFires)
+    fireTotDiffFit = 0.01 + fireSumFitness(droneCapac, ant.fireSum);
+    fireFitness = fireTotDiffFit;
     iDronePerFire = totalFires / totalDrones; %ideal drones per fire
-    fireFitness = fireFitness + fireEqFitness(iDronePerFire, length(ant.fires));
+    fireEqFit = fireEqFitness(iDronePerFire, length(ant.fires));
+    fireFitness = fireFitness + fireEqFit;
+    fireIntFit = 0;
     for f = 1: length(ant.fires)
-        fireFitness = fireFitness + fireIntFitness(droneCapac, iDronePerFire, ant.fires(f));
+        fireIntFit = fireIntFit + fireIntFitness(droneCapac, iDronePerFire, ant.fires(f));
     end
+    fireFitness = fireFitness + fireIntFit;
 end
--- Code/fireSumFitness.m
+++ Code/fireSumFitness.m
@@ -1,3 +1,3 @@
 function [fireSumFitness] = fireSumFitness (droneCapac, fireSum)
-   fireSumFitness = (abs(droneCapac - fireSum))^1;
+   fireSumFitness = floor((abs(droneCapac - fireSum))^1);
 end
