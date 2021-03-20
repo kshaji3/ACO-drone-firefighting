7ecfdf90a500731696dd5a68e09d529712179003
Karthik <Shaji>
2021-03-19 17:34:56 -0700

    Read excel tables functionality

--- Testing/MechanicsTesting.m
+++ Testing/MechanicsTesting.m
@@ -1,3 +1,6 @@
-clear
-colony(1).ant(1).tour(1) = 10;
-colony(2).ant(1).tour(1) = 20;
+t = readtable('../data-manipulation/northeast-samples.xlsx', 'sheet', 'Sheet1')
+t = table2cell(t)
+fireCoords = t(:, 8)
+xCoords = cell2mat(t(:, 15))
+yCoords = cell2mat(t(:, 16))
+plot(xCoords, yCoords)
