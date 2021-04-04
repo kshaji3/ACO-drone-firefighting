fd58bbe58190bb034c20a7f70c3912b62b0e02e6
Karthik <Shaji>
2021-04-03 21:38:18 -0700

    R modification
    Got rid of the rows which had elevation values of <null>. This prevents reliable data testing on a 3D graph, which I am building up right now

--- Code/createFires.m
+++ Code/createFires.m
@@ -1,5 +1,5 @@
 function [fires] = createFires()
-    t = table2cell(readtable('../data-manipulation/pascascades-samples.xlsx', 'sheet', 'Sheet1'));
+    t = table2cell(readtable('../data-manipulation/northeast-samples.xlsx', 'sheet', 'Sheet1'));
     %convert from cell to array and transpose it (')
     fires.intensity = cell2mat(t(:, 8))'; 
     %[1 1 1 2 2 2 3 3 3 4 4 4 5 5 5];
@@ -7,4 +7,5 @@ function [fires] = createFires()
     %[0 0 0 5 5 5 10 10 10 15 15 15 20 20 20];
     fires.locY = cell2mat(t(:, 16))' ./ (10^5);
     %[5 10 15 5 10 15 5 10 15 5 10 15 5 10 15];
+%     fires.locZ = cell2mat(t(:, 
 end
--- Testing/MechanicsTesting.m
+++ Testing/MechanicsTesting.m
@@ -1,7 +1,4 @@
-t = readtable('../data-manipulation/northeast-samples.xlsx', 'sheet', 'Sheet1')
-t = table2cell(t)
-
-fireCoords = cell2mat(t(:, 8))'
-xCoords = cell2mat(t(:, 15))'
-yCoords = cell2mat(t(:, 16))'
-plot(xCoords, yCoords)
+x = [1 2 3 4 5];
+y = [1 2 3 4 5];
+z = [1 2 3 4 5];
+plot3(x, y, z, 'o')
